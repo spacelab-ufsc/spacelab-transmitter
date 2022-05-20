d2ced3eba6e547d43bc5c23e73662433b3a97590
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2022-05-20 11:06:31 -0300

    Minor improvements in the GMSK class #31

--- spacelab_transmitter/gmsk.py
+++ spacelab_transmitter/gmsk.py
@@ -25,26 +25,33 @@ import numpy as np
 from scipy.signal import upfirdn, lfilter
 
 class GMSK:
-
+    """
+    GMSK modulator.
+    """
     def __init__(self, bt, baud):
+        """
+        Constructor.
+
+        :param bt: BT product (bandwidth x bit period) for GMSK
+        :param baud: The desired data rate in bps
+        """
         self._bt = bt
         self._baudrate = baud
 
     def modulate(self, data):
         """
-        Function to modulate a binary stream using GMSK modulation
-        Parameters:
-        data : input integer list to modulate (bytes as integers)
-        Returns:
-        (s_t,s_complex) : tuple containing the following variables
-        s_t : GMSK modulated signal with carrier s(t)
-        s_complex : baseband GMSK signal (I+jQ)
+        Function to modulate an integer stream using GMSK modulation.
+
+        :param data: input integer list to modulate (bytes as integers)
+
+        :return: s_t: GMSK modulated signal with carrier s(t)
+        :return: s_complex: baseband GMSK signal (I+jQ)
         """
         L = 40
         fc = self._baudrate                         # Carrier frequency = Data transfer rate in bps
         fs = L*fc
         Ts = 1/fs
-        Tb = L*Ts                                   # Derived waveform timing parameters
+        Tb = L*Ts                                   # Bit period in seconds
         data = self._int_list_to_bit_list(data)
         data = np.array(data)
         c_t = upfirdn(h=[1]*L, x=2*data-1, up = L)  # NRZ pulse train c(t)
@@ -52,7 +59,7 @@ class GMSK:
         h_t = self._gaussian_lpf(Tb, L, k)          # Gaussian LPF with BT=0.25
         b_t = np.convolve(h_t, c_t, 'full')         # Convolve c(t) with Gaussian LPF to get b(t)
         bnorm_t = b_t/max(abs(b_t))                 # Normalize the output of Gaussian LPF to +/-1
-        h = 0.5                                     # Modulation index (GMSK = 0,5)
+        h = 0.5                                     # Modulation index (GMSK = 0.5)
         # Integrate to get phase information
         phi_t = lfilter(b = [1], a=[1,-1], x=bnorm_t*Ts) * h*np.pi/Tb
         I = np.cos(phi_t)
@@ -63,17 +70,17 @@ class GMSK:
         sQ_t = Q*np.sin(2*np.pi*fc*t)
         s_t = sI_t - sQ_t                           # s(t) - GMSK with RF carrier
 
-        return (s_t, s_complex)
+        return s_t, s_complex
 
     def _gaussian_lpf(self, Tb, L, k):
         """
-        Generate filter coefficients of Gaussian low pass filter (used in gmsk_mod)
-        Parameters:
-        Tb : bit period
-        L : oversampling factor (number of samples per bit)
-        k : span length of the pulse (bit interval)
-        Returns:
-        h_norm : normalized filter coefficients of Gaussian LPF
+        Generate filter coefficients of Gaussian low pass filter (used in gmsk_mod).
+
+        :param Tb: bit period
+        :param L: oversampling factor (number of samples per bit)
+        :param k: span length of the pulse (bit interval)
+
+        :return h_norm: normalized filter coefficients of Gaussian LPF
         """
         B = self._bt/Tb     # Bandwidth of the filter
         # Truncated time limits for the filter
@@ -83,6 +90,13 @@ class GMSK:
         return h_norm
 
     def _int_list_to_bit_list(self, n):
+        """
+        Converts a integer list (bytes) to a bit list.
+
+        :param n: An integer list.
+
+        :return res: The given integer list as a bit list
+        """
         res = list()
         for i in n:
             res = res + [int(digit) for digit in bin(i)[2:].zfill(8)]
