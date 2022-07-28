550b942263ea35a78033aa89f264a773e7238984
vitoria <vitoriabbianchin@gmail.com>
2022-07-27 22:26:12 -0300

    Unit test for Payload Data Telecommand #73

--- tests/test_telecommands.py
+++ tests/test_telecommands.py
@@ -298,7 +298,7 @@ def test_tc_get_payload_data():
         hashed = hmac.new(key.encode('utf-8'), bytes(exp_pl), hashlib.sha1)
 
         #generate 
-        res = x.generate(src_adr, src_adr, pl_id, pl_args, key)
+        res = x.generate(src_adr, pl_id, pl_args, key)
         assert res == exp_pl + list(hashed.digest())
 
 def test_tc_data_request():
