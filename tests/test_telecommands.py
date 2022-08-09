73babe009f7efd7bc6ca5bce1d199b51bfd0796e
vitoria <vitoriabbianchin@gmail.com>
2022-08-09 20:24:36 -0300

    Developing datar Request unit test #70

--- tests/test_telecommands.py
+++ tests/test_telecommands.py
@@ -338,7 +338,7 @@ def test_tc_data_request():
         hashed = hmac.new(key.encode('utf-8'), bytes(exp_pl), hashlib.sha1)
 
         #generate 
-        res = x.generate(src_adr, data_id, start_ts ,end_ts_list, key)
+        res = x.generate(src_adr, data_id, start_ts ,end_ts, key)
         assert res == exp_pl + list(hashed.digest())
 
 def test_tc_get_parameter():
