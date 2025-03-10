02b32105fd34f598fd021f39398695a986946f40
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2025-03-10 00:42:04 -0300

    Improving the Satellite class

--- spacelab_transmitter/satellite.py
+++ spacelab_transmitter/satellite.py
@@ -20,6 +20,10 @@
 #
 #
 
+import json
+
+from spacelab_transmitter.link import Link
+
 class Satellite:
     """
     Satellite object.
@@ -30,6 +34,62 @@ class Satellite:
         """
         self._name = ""
         self._links = list()
+        self._link = None
+
+    def __str__(self):
+        """
+        String representation of the satellite.
+
+        :return: A text description of the satellite.
+        :rtype: str
+        """
+        txt = "Satellite: " + self.get_name() + "\n\r"
+        txt += "Links:" + "\n\r"
+        links = self.get_links()
+        for l in links:
+            txt += str(l)
+
+        return txt
+
+    def load_from_file(self, filename):
+        """
+        Loads the satellite parameters from a JSON file.
+
+        :param filename: Is the name of the file to load.
+        :type: str
+
+        :return: None
+        """
+        with open(filename) as f:
+            sat_info = json.load(f)
+
+            if 'name' in sat_info:
+                self.set_name(sat_info['name'])
+            else:
+                raise RuntimeError("The satellite configuration file is corrupted!")
+
+            if 'links' in sat_info:
+                links = list()
+                for i in range(len(sat_info['links'])):
+                    link = Link()
+
+                    link.set_id(sat_info['links'][i]['id'])
+                    link.set_name(sat_info['links'][i]['name'])
+                    link.set_direction(sat_info['links'][i]['direction'])
+                    link.set_frequency(int(sat_info['links'][i]['frequency']))
+                    link.set_modulation(sat_info['links'][i]['modulation'])
+                    link.set_baudrate(sat_info['links'][i]['baudrate'])
+                    link.set_preamble(sat_info['links'][i]['preamble'])
+                    link.set_sync_word(sat_info['links'][i]['sync_word'])
+                    link.set_link_protocol(sat_info['links'][i]['protocol_link'])
+                    link.set_network_protocol(sat_info['links'][i]['protocol_network'])
+                    link.set_packets(sat_info['links'][i]['packets'])
+
+                    links.append(link)
+
+                self.set_links(links)
+            else:
+                raise RuntimeError("The satellite configuration file is corrupted!")
 
     def set_name(self, name):
         """
@@ -60,7 +120,7 @@ class Satellite:
 
         :return: None
         """
-        self._links = links
+        self._links = links.copy()
 
     def get_links(self):
         """
@@ -70,3 +130,23 @@ class Satellite:
         :rtype: list
         """
         return self._links
+
+    def set_active_link(self, idx):
+        """
+        Sets the current active communication link.
+
+        :param idx: Is the index of the current active link.
+        :type: int
+
+        :return: None.
+        """
+        self._link = self.get_links()[idx]
+
+    def get_active_link(self):
+        """
+        Gets the active communication link.
+
+        :return: The current active link.
+        :rtype: Link
+        """
+        return self._link
