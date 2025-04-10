ea0e7a1851f34872b45a95cd6b7554f0b7eeebbd
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2025-04-10 13:36:32 -0300

    tests: Updating the unit test of the CSP class

--- tests/test_csp.py
+++ tests/test_csp.py
@@ -432,14 +432,15 @@ def test_encode_with_hmac():
     for i in range(random.randint(0, 2**16-1)):
         pl.append(random.randint(0, 2**8-1))
 
-    key = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=16))
-    key_hash = hashlib.sha1(key.encode('utf-8'))
-    hashed = hmac.new(key_hash.digest()[:16], bytes(pl), hashlib.sha1)
-    pl_hash = list(hashed.digest())
-
     csp = CSP(src_adr)
 
+    key = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=16))
+
     pkt = csp.encode(CSP_PRIO_NORM, dst_adr, src_port, dst_port, False, True, False, False, False, pl, key)
+
+    key_hash = hashlib.sha1(key.encode('utf-8'))
+    hashed = hmac.new(key_hash.digest()[:16], bytes(pkt[:4] + pl), hashlib.sha1)
+    pl_hash = list(hashed.digest())
 
     assert (pkt[0] >> 6) == 2                                       # Priority
     assert ((pkt[0] >> 1) & 31) == src_adr                          # Source address
@@ -493,16 +494,17 @@ def test_append_hmac():
     for i in range(random.randint(0, 2**16-1)):
         pl.append(random.randint(0, 2**8-1))
 
-    key = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=16))
-    key_hash = hashlib.sha1(key.encode('utf-8'))
-    hashed = hmac.new(key_hash.digest()[:16], bytes(pl), hashlib.sha1)
-    pl_hash = list(hashed.digest())
-
     csp = CSP(src_adr)
 
     pkt = csp.encode(CSP_PRIO_NORM, dst_adr, src_port, dst_port, False, False, False, False, False, pl)
 
+    key = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=16))
+
     pkt = csp.append_hmac(pkt, key)
+
+    key_hash = hashlib.sha1(key.encode('utf-8'))
+    hashed = hmac.new(key_hash.digest()[:16], bytes(pkt[:4] + pl), hashlib.sha1)
+    pl_hash = list(hashed.digest())
 
     assert (pkt[0] >> 6) == 2                                       # Priority
     assert ((pkt[0] >> 1) & 31) == src_adr                          # Source address
