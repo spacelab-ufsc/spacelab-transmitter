f17070b8aa2784bbf12f941b140d1236d977514b
Gabriel Mariano Marcelino <gabriel.mm8@gmail.com>
2025-11-02 16:16:04 -0300

    data: satellites: Updating the name of the FloripaSat-2A satellite

--- spacelab_transmitter/spacelabtransmitter.py
+++ spacelab_transmitter/spacelabtransmitter.py
@@ -96,7 +96,7 @@ _DEFAULT_LOGFILE                = 'logfile.csv'
 
 # Satellites
 _SATELLITES                     = [["FloripaSat-1", "floripasat-1.json"],
-                                   ["FloripaSat-2", "floripasat-2.json"],
+                                   ["FloripaSat-2A", "floripasat-2a.json"],
                                    ["GOLDS-UFSC", "golds-ufsc.json"],
                                    ["Catarina-A1", "catarina-a1.json"],
                                    ["Catarina-A2", "catarina-a2.json"]]
